f8ed54d0746c145c1183b9925114bdaab9214692
ettore <ettore.dallamutta@studenti.unipd.it>
2024-10-09 13:40:33 +0200

    add scripts files

---
+++ scripts/template/seasonal_battery_building.py
@@ -0,0 +1,51 @@
+from reho.model.reho import *
+from reho.plotting import plotting
+
+
+if __name__ == '__main__':
+
+    # Set building parameters
+    reader = QBuildingsReader()  # load QBuildingsReader class
+    reader.establish_connection('Geneva')  # connect to QBuildings database
+    qbuildings_data = reader.read_db(5, egid=['2034144/2034143/2749579/2034146/2034145'])  # read data
+
+    # Select clustering options for weather data
+    #  - I refers to Irradiance, T to Temperature, and W to Weekday
+    #  - specify the desired number of typical days
+    cluster = {'Location': 'Geneva', 'Attributes': ['T', 'I', 'W'], 'Periods': 12, 'PeriodDuration': 24}
+
+    # Set scenario
+    scenario = dict()
+    scenario['Objective'] = 'OPEX'  # select an objective function as defined in ampl_model/scenario.mod
+    scenario['EMOO'] = {}  # remain empty for now
+    scenario['specific'] = []  # remain empty for now
+    scenario['name'] = 'totex'  # any name is possible here
+    scenario['exclude_units'] = ['NG_Cogeneration','HeatPump_Air','HeatPump_Geothermal']  # specify some units to be excluded
+    scenario['enforce_units'] = ['HS_IP']  # specify some units to be enforced
+
+    # Initialize available units and grids
+    grids = infrastructure.initialize_grids()  # grids parameters are based on data/infrastructure/grids.csv
+    units = infrastructure.initialize_units(scenario, grids, storage_data= True)  # units are based on data/infrastructure/building_units.csv
+
+    # Set method options (as defined in sub_problem.py > initialize_default_methods)
+    # By default a district-scale design is performed with a compact formulation.
+    # Watch out the maximum number of buildings is around 10 due to exponential complexity.
+    method = {'use_Storage_Interperiod': True}
+
+    # Run optimization
+    reho = REHO(qbuildings_data=qbuildings_data, units=units, grids=grids, cluster=cluster, scenario=scenario, method=method, solver="gurobi")
+    reho.single_optimization()
+
+    # Save results
+    reho.save_results(format=['xlsx', 'pickle'], filename='0')
+
+    # Plot results
+    # plotting.plot_performance(reho.results, plot='costs', indexed_on='Scn_ID', label='EN_long').show()
+    # plotting.plot_performance(reho.results, plot='gwp', indexed_on='Scn_ID', label='EN_long').show()
+
+    units_to_plot = ['BESS_IP', 'HeatPump', 'PV']
+    #plotting.plot_energy_balance_for_battery(reho.results['totex'][0], units_to_plot, color='ColorPastel', day_of_the_year = 9,time_range='2weeks', label='EN_long').show()
+
+    plotting.plot_storage_profile(reho.results['totex'][0], resolution='daily').show()
+
+    #plotting.plot_sankey(reho.results['totex'][0], label='EN_long', color='ColorPastel').show()
---
+++ scripts/template/seasonal_battery_district.py
@@ -0,0 +1,35 @@
+from reho.model.reho import *
+from reho.plotting import plotting
+
+
+if __name__ == '__main__':
+
+    # Set building parameters
+    reader = QBuildingsReader()
+    reader.establish_connection('Geneva')
+    qbuildings_data = reader.read_db(transformer=234, egid=['1017073/1017074', '1017109', '1017079', '1030377/1030380']) #, '1017079', '1030377/1030380'
+
+    # Select clustering options for weather data
+    cluster = {'Location': 'Geneva', 'Attributes': ['T', 'I', 'W'], 'Periods': 10, 'PeriodDuration': 24}
+
+    # Set scenario
+    scenario = dict()
+    scenario['Objective'] = 'OPEX'
+    scenario['name'] = 'totex'
+    scenario['exclude_units'] = ['NG_Cogeneration','PV','BESS_IP']
+    scenario['enforce_units'] = ['BESS_IP_district']
+
+    # Initialize available units and grids
+    grids = infrastructure.initialize_grids()
+    units = infrastructure.initialize_units(scenario, grids, district_data = True) #, storage_data= True  ,'use_Storage_Interperiod': True
+
+    # Set method options
+    method = {'district-scale': True}
+
+
+    # Run optimization
+    reho = REHO(qbuildings_data=qbuildings_data, units=units, grids=grids, cluster=cluster, scenario=scenario, method=method, solver="gurobi")
+    reho.single_optimization()
+
+    # Save results
+    reho.save_results(format=['xlsx', 'pickle'], filename='0')
