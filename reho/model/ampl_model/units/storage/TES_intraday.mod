11d5bddd6bbd78b2713dfd102fbe02668785741e
ravi <sai.ravi@epfl.ch>
2025-03-19 00:56:49 +0100

    think DHN tank works now

--- reho/model/ampl_model/units/storage/TES_intraday.mod
+++ reho/model/ampl_model/units/storage/TES_intraday.mod
@@ -4,22 +4,20 @@
 #--------------------------------------------------------------------------------------------------------------------#
 ######################################################################################################################
 
+param ITES_eff_ch{u in UnitsOfType['DHN_tank']} 	default 0.9;		#-	[1]
+param ITES_eff_di{u in UnitsOfType['DHN_tank']} 	default 0.9;
+param ITES_limit_ch{u in UnitsOfType['DHN_tank']} default 0.8;			#-	[2] max charging limit
+param ITES_limit_di{u in UnitsOfType['DHN_tank']} default 0.2;			#-	[1] max discharging limit
+param ITES_efficiency{u in UnitsOfType['DHN_tank']} default 0.99997;
 
-param ITES_limit_ch{u in UnitsOfType['DHN_tank']} default 1;			#-	[2] max charging limit
-param ITES_limit_di{u in UnitsOfType['DHN_tank']} default 0;			#-	[1] max discharging limit
-param ITES_self_discharge{u in UnitsOfType['DHN_tank']} default 0.9999729;	#-	[1]
-param ITES_efficiency{u in UnitsOfType['DHN_tank']} default 0.97128;
 var ITES_E_stored{u in UnitsOfType['DHN_tank'],p in Period,t in Time[p]} >= 0;
-
-var mode_charge_ITES{u in UnitsOfType['DHN_tank'], p in Period, t in Time[p]} binary := 0;
-var mode_discharge_ITES{u in UnitsOfType['DHN_tank'], p in Period, t in Time[p]} binary := 0;
-param ITES_mode := 1e6;
 
 #--Hourly Energy balance (valid for intra-period storage)
 subject to ITES_energy_balance{u in UnitsOfType['DHN_tank'], p in Period,t in Time[p] diff {last(Time[p])}}:
-(ITES_E_stored[u,p,next(t,Time[p])] - ITES_self_discharge[u]*ITES_E_stored[u,p,t]) =
-	(ITES_efficiency[u]*Units_demand['Heat',u,p,t]
-	- (1/ITES_efficiency[u])*Units_supply['Heat',u,p,t] )*dt[p];
+(ITES_E_stored[u,p,next(t,Time[p])] - ITES_efficiency[u]*ITES_E_stored[u,p,t]) = 
+	( ITES_eff_ch[u]*Units_demand['Heat',u,p,t] - (1/ITES_eff_di[u])*Units_supply['Heat',u,p,t] )*dt[p];
+
+
 
 #--SoC constraints
 subject to ITES_c1{u in UnitsOfType['DHN_tank'], p in Period,t in Time[p]}:
@@ -28,14 +26,15 @@ ITES_E_stored[u,p,t] <= ITES_limit_ch[u]*Units_Mult[u];
 subject to ITES_c2{u in UnitsOfType['DHN_tank'], p in Period,t in Time[p]}:
 ITES_E_stored[u,p,t] >= ITES_limit_di[u]*Units_Mult[u];
 
+
+subject to ITES_c3{u in UnitsOfType['DHN_tank'] ,p in Period,t in Time[p]}:
+Units_demand['Heat',u,p,t]*dt[p] <= (ITES_limit_ch[u]-ITES_limit_di[u])*Units_Mult[u];
+
+subject to ITES_c4{u in UnitsOfType['DHN_tank'],p in Period,t in Time[p]}:
+Units_supply['Heat',u,p,t]*dt[p] <= (ITES_limit_ch[u]-ITES_limit_di[u])*Units_Mult[u];
+
 #--Cyclic
 subject to ITES_E_stored_cyclic{u in UnitsOfType['DHN_tank'],p in Period}:
- ITES_E_stored[u,p,first(Time[p])] = ITES_self_discharge[u]*ITES_E_stored[u,p,last(Time[p])] +(ITES_efficiency[u]*Units_demand['Heat',u,p,last(Time[p])] - (1/ITES_efficiency[u])*Units_supply['Heat',u,p,last(Time[p])])*dt[p];
+(ITES_E_stored[u,p,first(Time[p])] - ITES_efficiency[u]*ITES_E_stored[u,p,last(Time[p])]) =
+	(ITES_eff_ch[u]*Units_demand['Heat',u,p,last(Time[p])] - (1/ITES_eff_di[u])*Units_supply['Heat',u,p,last(Time[p])])*dt[p];
 
-
-param delta_T_ITES{u in UnitsOfType['DHN_tank']} default 60;
-
-
-# constraint to impose the minimum allowed temperature inside the storage
-subject to min_T_allowed{u in UnitsOfType['DHN_tank'], p in Period,t in Time[p]}:
-	Units_Mult[u]*Text_Geneva[p,t]+ITES_E_stored_IP[u,p,t]*delta_T_ITES[u] >= Units_Mult[u]*22.5;
--- scripts/template/EPFL_ORC.py
+++ scripts/template/EPFL_ORC.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     # Set building parameters
     # you can as well define your district from a csv file instead of reading the database
     reader = QBuildingsReader()
-    n_house = 26
+    n_house = 4
     file_ID = "/EPFL_MOES.csv"
     epfl_csv_path = path_to_buildings_csv + file_ID
     qbuildings_data = reader.read_csv(buildings_filename=epfl_csv_path, nb_buildings= n_house)
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     cluster = {'Location': 'Pully', 'Attributes': ['I', 'T','E','D','CS'], 'Periods': 14, 'PeriodDuration': 24}
     attributes = ['Irr', 'Text', 'Emissions','DataLoad','Cost_supply_elec']
     weather_file = path_to_profiles + '/pully.csv'
-    weather.data_centre_profile(size = 10000)
+    weather.data_centre_profile(size = 8000)
     df_annual = weather.read_custom_weather(weather_file, weeks = cluster['PeriodDuration'] ==168)
     df_annual = df_annual[attributes]
     nb_clusters = [cluster['Periods']]
@@ -31,17 +31,17 @@ if __name__ == '__main__':
 
     scenario = dict()
 
-    scenario['Objective'] = ['OPEX', 'CAPEX']
-    scenario['nPareto'] = 4
+    #scenario['Objective'] = ['OPEX', 'CAPEX']
+    #scenario['nPareto'] = 4
     #scenario['name'] = 'gwp'
 
-    #scenario['Objective'] = 'TOTEX'
+    scenario['Objective'] = 'OPEX'
     scenario['name'] = 'gwp'
-    scenario['exclude_units'] = ['HeatPump_Geothermal','HeatPump_Air','HeatPump_Lake','HeatPump_Anergy', 'ThermalSolar', 'Battery','STES_district','HeatPump_DataCentre_district', 'DataHeat_DHW', 'DataHeat_SH','TES_intraday_district'] #'OIL_Boiler',  'NG_Boiler','HeatPump_Air', 'HeatPump_Lake''HeatPump_Anergy''DataHeatSH',
+    scenario['exclude_units'] = ['HeatPump_Geothermal','HeatPump_Air','HeatPump_Lake','HeatPump_Anergy', 'ThermalSolar', 'Battery','STES_district','HeatPump_DataCentre_district', 'DataHeat_DHW', 'DataHeat_SH'] #'OIL_Boiler',  'NG_Boiler','HeatPump_Air', 'HeatPump_Lake''HeatPump_Anergy''DataHeatSH',
 
     #
     #
-    scenario['enforce_units'] = ['ORC_EPFL_district','Battery_district'] #'HeatPump_Geothermal_district','DHN_out_district','Battery_district','PV_district',
+    scenario['enforce_units'] = ['ORC_EPFL_district','Battery_district', 'TES_intraday_district'] #'HeatPump_Geothermal_district','DHN_out_district','Battery_district','PV_district',
     scenario["specific"] = ["enforce_DHN"]
 
     # Initialize available units and grids
@@ -54,14 +54,14 @@ if __name__ == '__main__':
 
     parameters = {'n_vehicles': np.array([0.0]), 'T_DHN_supply_cst': np.repeat(67.0, n_house),'T_DHN_return_cst': np.repeat(55.0, n_house) ,'data_EUD': data_centre_heat_profile, 'TransformerCapacity': np.array([1e8, 1e8,0,0]),'T_ext' : temp_profile , 'I_global': irr_profile } #, 'Network_supply_heat': np.array([0.0])
     #parameters = {}
-    DW_params = {'max_iter': 2}
+    #DW_params = {'max_iter': 2}
 
     # Set method options
-    method = {'district-scale': True,'save_stream_t': True, 'use_dynamic_emission_profiles': True, 'save_streams': True, 'ORC_all_the_time': False,"Link_DC_to_district_PV": True} #, 'use_pv_orientation': True
+    method = {'building-scale': True,'save_stream_t': True, 'use_dynamic_emission_profiles': True, 'save_streams': True, 'ORC_all_the_time': False,"Link_DC_to_district_PV": True} #, 'use_pv_orientation': True
     # Run optimization
-    reho = REHO(qbuildings_data=qbuildings_data, units=units,parameters=parameters, grids=grids, cluster=cluster, scenario=scenario, method=method, solver ='gurobi', DW_params=DW_params) #parameters=parameters,
-    reho.generate_pareto_curve()
-    #reho.single_optimization()
+    reho = REHO(qbuildings_data=qbuildings_data, units=units,parameters=parameters, grids=grids, cluster=cluster, scenario=scenario, method=method, solver ='gurobi') #parameters=parameters,, DW_params=DW_params
+    #reho.generate_pareto_curve()
+    reho.single_optimization()
 
     '''
     SA = SensitivityAnalysis(reho, SA_type="Monte_Carlo", sampling_parameters= 8)
@@ -73,8 +73,8 @@ if __name__ == '__main__':
     #plotting.plot_composite_curve(reho.results["totex"][0], cluster, plot= True, periods =["Yearly"]) #,"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
     #plotting.yearly_demand_plot(reho.results["totex"][0], cluster, plot=True)
     # Save results
-    filename='EPFL_ORC_Pareto_PV_ICT_linked'
-    reho.save_results(format=['pickle'], filename=filename)
+    filename='tank_check'
+    reho.save_results(format=['xlsx','pickle'], filename=filename)
     #plotting.plot_performance(reho.results, plot='costs', indexed_on='Scn_ID', label='EN_long').show()
     #plotting.plot_performance(reho.results, plot='gwp', indexed_on='Scn_ID', label='EN_long').show()
     #plotting.plot_sankey(reho.results['gwp'][0], label='EN_long', color='ColorPastel').show()
